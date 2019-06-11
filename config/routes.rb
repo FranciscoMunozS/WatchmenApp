0dda158371e90022e31fe9f7b2ae7adc9c49c060
Francisco <franciscomuniozs@gmail.com>
2019-06-11 15:31:30 -0400

    Last update - fixed routes

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,6 @@ Rails.application.routes.draw do
   resources :documents
   devise_for :users
   resources :providers
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root 'tickets#index'
 end
