a10e21ecf16c3b70b97d684b819ebfc2df07904a
Francisco <franciscomuniozs@gmail.com>
2019-06-19 12:42:40 -0400

    Last update - Custom mail format

--- test/mailers/previews/new_record_mailer_preview.rb
+++ test/mailers/previews/new_record_mailer_preview.rb
@@ -1,4 +1,6 @@
 # Preview all emails at http://localhost:3000/rails/mailers/new_record_mailer
 class NewRecordMailerPreview < ActionMailer::Preview
-
+  def new_record_preview
+    NewRecordMailer.new_record_email(Ticket.first)
+  end
 end
