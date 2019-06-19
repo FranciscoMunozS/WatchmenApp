677664984fc6b50565cd314e6a1efb8edf1aa492
Francisco <franciscomuniozs@gmail.com>
2019-06-19 14:55:59 -0400

    Last update - added validations for email deployment

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -28,11 +28,15 @@ class Ticket < ApplicationRecord
 
   def first_call_email
     self.first_call = (first_notification.to_date - Date.today).to_i
-    SendNotificationJob.set(wait: self.first_call.days).perform_later(self)
+    if self.first_call_changed?
+      SendNotificationJob.set(wait: self.first_call.days).perform_later(self)
+    end
   end
 
   def second_call_email
     self.second_call = (second_notification.to_date - Date.today).to_i
-    SendSecondNotificationJob.set(wait: self.second_call.days).perform_later(self)
+    if self.first_call_changed?
+      SendSecondNotificationJob.set(wait: self.second_call.days).perform_later(self)
+    end
   end
 end
