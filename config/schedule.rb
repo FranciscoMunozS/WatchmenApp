d4610239bbe76a8c31b7be3ee5cd03555451d919
Francisco <franciscomuniozs@gmail.com>
2019-08-14 10:41:51 -0400

    Last update - cron update

--- config/schedule.rb
+++ config/schedule.rb
@@ -3,6 +3,6 @@ set :environment, :production
 env :PATH, ENV['PATH']
 job_type :rbenv_rake, %q!eval "$(rbenv init -)"; cd :path && :environment_variable=:environment bundle exec rake :task --silent :output!
 
-every 1.month do
+every 1.minute do
   rake 'send_digest_email'
 end
